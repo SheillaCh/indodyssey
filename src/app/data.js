d0fa654bf422938d7acbd8b09be06c52bbf71b10
Sheilla Chairiandita <workshe15@gmail.com>
2024-07-11 14:23:45 +0700

    Upload data

---
+++ src/app/data.js
@@ -0,0 +1,36 @@
+export const provinces = [
+    { provName: "Nanggore Aceh Darussalam",
+      provCapital: "", 
+      provArea: "", 
+      provPopulation: "", 
+      provNumOfCity: 2, 
+      provLang:"",
+      attraction: [
+        {attractionName: " ",attractionDescription:" ", attractionAccess: " ",attractionImage: "", attractionMap:" ", attractionSource: " ",
+          attractionTips: ["",""]},
+        {attractionName: " ",attractionDescription:" ", attractionAccess: " ",attractionImage: "", attractionMap:" ", attractionSource: " ",
+          attractionTips: ["",""]},
+        {attractionName: " ",attractionDescription:" ", attractionAccess: " ",attractionImage: "", attractionMap:" ", attractionSource: " ",
+          attractionTips: ["",""]}
+      ],
+      souvenir: [
+        {souvenirName: " ",souvenirDescription:" ", souvenirImage: "", souvenirMap:" ",souvenirRate: 4, souvenirSource:" "},
+        {souvenirName: " ",souvenirDescription:" ", souvenirImage: "", souvenirMap:" ",souvenirRate: 4, souvenirSource:" "},
+        {souvenirName: " ",souvenirDescription:" ", souvenirImage: "", souvenirMap:" ",souvenirRate: 4, souvenirSource:" "},
+      ],
+      funfacts: [
+        {funfact:" ",funfactImage:"", funfactSource: " "},
+        {funfact:" ",funfactImage:"", funfactSource: " "},
+      ],
+      food: [
+        {foodName: " ",foodRecipe:" ",foodImage: "", foodSource: ""},
+        {foodName: " ",foodRecipe:" ",foodImage: "", foodSource: ""},
+        {foodName: " ",foodRecipe:" ",foodImage: "", foodSource: ""}
+      ],
+      restaurant: [
+        {restaurantName: " ",restaurantDescription:" ", restaurantImage: "", restaurantMap:" ", restaurantRate:4, restaurantSource: " "},
+        {restaurantName: " ",restaurantDescription:" ", restaurantImage: "", restaurantMap:" ", restaurantRate:4, restaurantSource: " "},
+        {restaurantName: " ",restaurantDescription:" ", restaurantImage: "", restaurantMap:" ", restaurantRate:4, restaurantSource: " "}
+      ],
+    }
+]
