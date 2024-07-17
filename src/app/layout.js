937ed8575abf17db6bbb29da3a04fb8a35dacb7b
Ninzato <ridzakalimanto@gmail.com>
2024-07-17 13:23:43 +0700

    Edit global.css file (deleting variables), layout.js (adding padding on all pages)

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import { cn } from "@lib/utils";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn("pl-[72px] pr-[72px]", inter.className)}>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,12 @@
+import NavBar from "@components/NavBar";
+
 export default function Home() {
   return (
-    <div className="flex justify-center">
-      <h1 className="text-6xl">Free Canvas</h1>
+    <div>
+      <div className="flex justify-center">
+        <h1 className="text-6xl">Free Canvas</h1>
+      </div>
+      <NavBar></NavBar>
     </div>
   );
 }
